b5e26ee8c3ddf58bef07d1713a24c69f4da62b01
Taiyob <oli1412001@gmail.com>
2023-11-10 17:08:57 +0600

    Home page design finished with route

--- donation-campaign/src/components/Header/Navbar/Navbar.jsx
+++ donation-campaign/src/components/Header/Navbar/Navbar.jsx
@@ -8,42 +8,42 @@ const Navbar = () => {
       <nav className="flex justify-between items-center py-6 px-5 max-w-[1300px] mx-auto mb-5">
         <Logo></Logo>
         <ul className="flex gap-5">
-          <li>
+          <li className='text-2xl'>
             <NavLink
               to="/"
               className={({ isActive, isPending, }) =>
                 isPending
                   ? 'pending'
                   : isActive
-                  ? "text-green-400 underline"
+                  ? "text-red-400 underline font-semibold text-2xl"
                   : ''
               }
             >
               Home
             </NavLink>
           </li>
-          <li>
+          <li className='text-2xl'>
             <NavLink
               to="/donation"
               className={({ isActive, isPending }) =>
                 isPending
                   ? "pending"
                   : isActive
-                  ? "text-green-400 underline"
+                  ? "text-red-400 underline font-semibold text-2xl"
                   : ""
               }
             >
               Donation
             </NavLink>
           </li>
-          <li>
+          <li className='text-2xl'>
             <NavLink
               to="/eee"
               className={({ isActive, isPending }) =>
                 isPending
                   ? "pending"
                   : isActive
-                  ? "text-green-400 underline"
+                  ? "text-red-400 underline font-semibold text-2xl"
                   : ""
               }
             >
